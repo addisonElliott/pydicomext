6750e709d53abdb1ff8326edcdac84dc63e54a3f
Addison Elliott <addison.elliott@gmail.com>
2018-12-06 13:52:23 -0600

    Pretty decent code for sorting slices
    Not fully tested yet but did a bit of testing and everything seems to work as expected.
    
    Small things to fix with a few method types

--- sortSlices.py
+++ sortSlices.py
@@ -1,7 +1,8 @@
 import logging
 import operator
-
 import pydicom
+
+import numpy as np
 
 from .util import *
 
@@ -9,7 +10,7 @@ logger = logging.getLogger(__name__)
 pydicom.config.datetime_conversion = True
 
 
-def sortSlices(series, method=MethodType.Unknown, reverse=False):
+def sortSlices(series, methods=MethodType.Unknown, reverse=False):
     """Sorts datasets in series based on its metadata
 
     Sorting the datasets within the series can be done based on a number of parameters, which are primarily going to be
@@ -19,7 +20,7 @@ def sortSlices(series, method=MethodType.Unknown, reverse=False):
     ----------
     series : Series
         [description]
-    method : MethodType, optional
+    method : MethodType or list(MethodType), optional
         [description] (the default is MethodType.Unknown, which [default_description])
     reverse : bool, optional
         Whether or not to reverse the sort, where the default sorting order is ascending (the default is False)
@@ -27,11 +28,62 @@ def sortSlices(series, method=MethodType.Unknown, reverse=False):
 
     if len(series) == 0:
         raise TypeError('Series must contain at least one dataset')
-    
-    # TODO Get best method to use when sorting the slices
-    # TODO This is going to be different for multiframe DICOM vs regular DICOM, general idea is the same though
 
-    pass
+    if methods == MethodType.Unknown:
+        methods = getBestMethod(series)
+
+    # Make a list out of the method if it is not one
+    if not isinstance(methods, list):
+        methods = [methods]
+
+    attrs = []
+    for method in methods:
+        # Check all of the specified methods for any invalid ones
+        if not isMethodValid(series, method):
+            raise TypeError('Invalid method specified: %s' % method)
+
+        if method == MethodType.SliceLocation:
+            attrs.append([d.SliceLocation for d in series])
+        elif method == MethodType.PatientLocation:
+            # Get slice positions for each object
+            sliceCosines, slicePositions = slicePositionsFromPatientInfo(series)
+
+            attrs.append(slicePositions)
+        elif method == MethodType.TriggerTime:
+            attrs.append([d.TriggerTime for d in series])
+        elif method == MethodType.AcquisitionDateTime:
+            attrs.append([d.AcquisitionDateTime for d in series])
+        elif method == MethodType.ImageNumber:
+            attrs.append([d.InstanceNumber for d in series])
+        elif method == MethodType.StackID:
+            attrs.append([int(d.FrameContentSequence[0].StackID) for d in series])
+        elif method == MethodType.StackPosition:
+            attrs.append([d.FrameContentSequence[0].InStackPositionNumber for d in series])
+        elif method == MethodType.TemporalPositionIndex:
+            attrs.append([d.FrameContentSequence[0].TemporalPositionIndex for d in series])
+        elif method == MethodType.FrameAcquisitionNumber:
+            attrs.append([d.FrameContentSequence[0].FrameAcquisitionNumber for d in series])
+        elif method == MethodType.MFPatientLocation:        
+            # TODO Do me
+            sliceCosines, slicePositions = slicePositionsFromPatientInfo(series)
+            attrs.append(slicePositions)
+        elif method == MethodType.MFAcquisitionDateTime:
+            attrs.append([d.FrameContentSequence[0].MFAcquisitionDateTime for d in series])
+        elif method == MethodType.CardiacTriggerTime:
+            attrs.append([d.CardiacSynchronizationSequence[0].NominalCardiacTriggerDelayTime for d in series])
+        elif method == MethodType.CardiacPercentage:
+            attrs.append([d.CardiacSynchronizationSequence[0].NominalPercentageOfCardiacPhase for d in series])
+
+    attrs.append(series)
+    sortedAttrs = list(zip(*sorted(zip(*attrs), reverse=reverse)))
+
+    sortedSeries = sortedAttrs[-1]
+
+    sortedAttrs = sortedAttrs[:-1]
+    diffs = list(map(np.diff, sortedAttrs))
+
+    # TODO This is going to be different for multiframe DICOM vs regular DICOM, general idea is the same though
+    return sortedSeries, diffs
 
 
 def sortSlices2(datasets, method=MethodType.Unknown, reverse=False):
