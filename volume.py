18eeb3855e9abeb9938fa6ab956edb3d6043a614
Addison Elliott <addison.elliott@gmail.com>
2018-12-08 08:09:47 -0600

    Commit changes

--- volume.py
+++ volume.py
@@ -1,6 +1,6 @@
 class Volume():
-    def __init__(self, volume=None, space=None, orientation=None, origin=None, spacing=None):
-        self.volume = volume
+    def __init__(self, data=None, space=None, orientation=None, origin=None, spacing=None):
+        self.data = data
         self.space = space
         self.orientation = orientation
         self.origin = origin
@@ -13,7 +13,7 @@ class Volume():
     Origin: %s
     Spacing: %s
     Volume shape: %s""" % (self.space, self.orientation, self.orientation, self.spacing,
-                           self.volume.shape if self.volume else None)
+                           self.data.shape if self.data else None)
 
     def __repr__(self):
         return self.__str__()
