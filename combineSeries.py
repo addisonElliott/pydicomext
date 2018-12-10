ae32cfc1adbed7d729f8b62b6cdd747876b589c7
Addison Elliott <addison.elliott@gmail.com>
2018-12-10 15:28:30 -0600

    Think I got the combineSeries function done
    Need to test it now!

--- combineSeries.py
+++ combineSeries.py
@@ -14,36 +14,62 @@ def combineSeries(series, methods=MethodType.Unknown, reverse=False, squeeze=Fal
         Origin, spacing, coordinate system, and orientation.
     """
 
+    # TODO Redo the return documentation on everything, split into multiple apparently
+    # TODO Redo this docstring
+
+    # TODO Swap rows/columns of pixel array or give option to do this
+    # Nope, we are going to use C-order indexing which is what pydicom abides by.
+    # Thus, shape of image is (frame, row, col) or (frame, y, x)
+    # This means any multidimensional info should be in front like:
+    # (t, z, y, x)
+    # Plenty of libraries in Python are using C-ordered indexing which Numpy defaults to so I want to stick with that.
+    # A simple transpose and reversal of data will work.
+    # Explain the C-ordering in docstring
+
+    # If the series has not been sorted yet, then sort it
+    # Or, if there are no datasets in the series then throw an error
     if series._shape is None:
         series = series.sort(methods, reverse, squeeze, warn, shapeTolerance, spacingTolerance)
     elif len(series) == 0:
         raise TypeError('Series must contain at least one dataset')
 
+    # List of variables that are stored from DICOM header
     imageShapes = []
     imageSpacings = []
-    imageThicknesses = []
-    imageSliceSpacings = []
     imageOrientations = []
+    origin = None
     images = []
 
     if series.isMultiFrame:
+        # Foreach dataset, store image shape, spacing, orientation and the image data itself
+        # The image spacing is not required in the DICOM header, so it will default to (1, 1) if not available
         for dataset in series:
             imageShapes.append(dataset.parent.pixel_array.shape[1:])
             imageSpacings.append(dataset.PixelMeasuresSequence[0].PixelSpacing if 'PixelSpacing' in
                                  dataset.PixelMeasuresSequence[0] else (1, 1))
             imageOrientations.append(dataset.PlaneOrientationSequence[0].ImageOrientationPatient)
             images.append(dataset.parent.pixel_array[dataset.sliceIndex, None, :, :])
+
+        # Origin for volume is the first series position
+        origin = np.asfarray(series[0].PlanePositionSequence[0].ImagePositionPatient)
     else:
+        # Foreach dataset, store image shape, spacing, orientation and the image data itself
+        # The image spacing is not required in the DICOM header, so it will default to (1, 1) if not available
         for dataset in series:
             imageShapes.append(dataset.pixel_array.shape)
             imageSpacings.append(dataset.PixelSpacing if 'PixelSpacing' in dataset else (1, 1))
             imageOrientations.append(dataset.ImageOrientationPatient)
             images.append(dataset.pixel_array[None, :, :])
 
+        # Origin for volume is the first series position
+        origin = np.asfarray(series[0].ImagePositionPatient)
+
+    # Check all of the image shapes to make sure they can be stacked together
     if not np.allclose(imageShapes, imageShapes[0], rtol=0.01):
         logger.debug('Datasets shape: %s' % imageShapes)
         raise Exception('Datasets do not have the same shape. Unable to combine into one volume')
 
+    # Check image spacings to make sure they are uniform, otherwise throw warning/error
     if not np.allclose(imageSpacings, imageSpacings[0], rtol=0.1):
         if warn:
             logger.warning('Datasets image spacing are not uniform')
@@ -52,6 +78,9 @@ def combineSeries(series, methods=MethodType.Unknown, reverse=False, squeeze=Fal
             logger.debug('Datasets spacings: %s' % imageSpacings)
             raise Exception('Datasets image spacing are not uniform')
 
+    # Check image orientations to make sure they are the same, otherwise throw warning/error
+    # I cannot think of a use case where there would be different image orientations that would be combined into one
+    # volume. It is a weird idea.
     if not np.allclose(imageOrientations, imageOrientations[0], rtol=0.1):
         if warn:
             logger.warning('Datasets image orientation are not the same')
@@ -65,26 +94,19 @@ def combineSeries(series, methods=MethodType.Unknown, reverse=False, squeeze=Fal
     imageSpacing = imageSpacings[0]
     imageOrientation = imageOrientations[0]
 
-    # Tack on the multidimensional shape and spacing calculated from sort series
+    # Get the entire shape of the data by taking the multidimensional shape and spacing and tack on the image size and
+    # spacing
     # This is prepended because we are using C-ordering meaning slower varying indices come first
-    imageShape = series.shape + imageShape
-    imageSpacing = series.spacing + imageSpacing
+    shape = series.shape + imageShape
+    spacing = series.spacing + tuple(imageSpacing)
 
     # Create 3D volume from stack of 2D images
     # Reshape the volume into the image shape
     volume = np.vstack(images)
-    volume = volume.reshape(imageShape)
+    volume = volume.reshape(shape)
 
     # DICOM uses LPS space
     space = 'left-posterior-superior'
-
-    # TODO Swap rows/columns of pixel array or give option to do this
-    # Nope, we are going to use C-order indexing which is what pydicom abides by.
-    # Thus, shape of image is (frame, row, col) or (frame, y, x)
-    # This means any multidimensional info should be in front like:
-    # (t, z, y, x)
-    # Plenty of libraries in Python are using C-ordered indexing which Numpy defaults to so I want to stick with that.
-    # A simple transpose and reversal of data will work.
 
     # Row cosines is first 3 elements, column cosines is last 3 elements of array, compute z cosines from row/col
     rowCosines = np.array(imageOrientation[:3])
@@ -92,23 +114,12 @@ def combineSeries(series, methods=MethodType.Unknown, reverse=False, squeeze=Fal
     zCosines = np.cross(rowCosines, colCosines)
 
     # Orientation is combination of the three cosines direction matrix
+    # Note: If the user sorts based on (z) location and sets reverse to True, then the third (z) column of orientation
+    # will need to be inverted to accurately reflect the orientation. No metadata for if the series is reverse sorted
+    # is not stored and I don't think it is worth storing. Rather, I have decided to leave it up to the user to change
+    # that last dimension if necessary. Until there is an valid application where reverse is used then I won't bother
     orientation = np.hstack((rowCosines, colCosines, zCosines))
 
-    # # TODO Check that all of the images are the same size first!
-    # # TODO Stack them together and reshape them
-    # # TODO Get space, orientation, pixel spacing, origin all setup!
-    # # TODO THink about reverse when doing this stuff
-    # # TODO Create a Volume class that holds all this information!
-
-    # # Get 3D volume from list of datasets
-    # volume = np.dstack((x.pixel_array for x in sortedDataset))
-
-    # space = 'left-posterior-superior'
-    # # Append the Z cosines to image orientation and then resize into 3x3 matrix
-    # orientation = np.append(np.asfarray(series[0].ImageOrientationPatient), sliceCosines).reshape((3, 3))
-    # # Concatenate (x,y) spacing list and zSpacing list
-    # spacing = np.append(np.asfarray(series[0].PixelSpacing), zSpacing)
-    # origin = np.asfarray(series[0].ImagePositionPatient)
-
-    # return method, space, orientation, spacing, origin, volume
-    return Volume(volume, space, orientation)
+    # Return Volume class containing information about the volume
+    # It's a basic wrapper class to contain any relevant data for the volume
+    return Volume(volume, space, orientation, origin, spacing)
